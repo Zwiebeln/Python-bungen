b9c4d19d1b37dbbe090368972be154cbce7dcb5a
Zwiebeln <ZhouJiexin@shuutekiAir.fritz.box>
2017-02-22 22:36:47 +0100

    Set up the the first 2 ex

---
+++ ex1.py
@@ -0,0 +1,9 @@
+print "Hallo Welt!"
+print "Hello again"
+print "I don't like typing this"
+print "But I have to"
+print "Or maybe I can make some a joke here"
+print "the joke consists two sentences"
+print "the first 'one'"
+print 'Bist du gut darauf?'
+print '"Bist du gut" darunten?'
---
+++ ex2.py
@@ -0,0 +1,12 @@
+# Add comments in the code.
+# How to write multiple lines?
+
+print "I could have code like this"
+
+# Comment can be used to disable code
+
+#print "This won't run"
+print "This will run"
+
+##how about use two octothorpes/ pound characters?
+##Also works :)
